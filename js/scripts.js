67f9834a8fd6c85ca91dcffddd1c47684b61bc16
Grace Mekarski & Ron Park <student@epicodus.com>
2015-02-11 17:18:47 -0800

    works to 100

--- js/scripts.js
+++ js/scripts.js
@@ -3,67 +3,8 @@ var numbersInWords = function(numberEntered) {
   var ordinals = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine", 10: "ten",
   11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"};
   var tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety"};
-  var biggies = {100: "hundred", 1000: "thousand", 1000000: "million", 1000000000: "billion"};
+  var biggies = {0: "hundred", 1: "thousand", 2: "million", 3: "billion"};
   var result = [];
-
-  // if (numberEntered < 1000000000 && numberEntered >= 2000000) {
-  //   var sevenDigit = Math.floor(numberEntered / 1000000);
-  //   result.push(tens[sevenDigit]);
-  //   var sixDigit = Math.floor((numberEntered % 1000000)/1000000);
-  //   var remainder = numberEntered % 1000000;
-  //   if (sixDigit !== 0) {
-  //     result.push(ordinals[sixDigit]);
-  //   }
-  //   result.push("million");
-  //   numberEntered = remainder;
-  // }
-  //
-  // if (numberEntered < 2000000 && numberEntered > 999999) {
-  //   var sixDigit = Math.floor(numberEntered / 1000000);
-  //   var remainder = numberEntered % 1000000;
-  //   if (sixDigit !== 0) {
-  //     result.push(ordinals[sixDigit]);
-  //   }
-  //   result.push("million");
-  //   numberEntered = remainder;
-  // }
-  //
-  // if (numberEntered < 1000000 && numberEntered >= 20000) {
-  //   var fiveDigit = Math.floor(numberEntered / 10000);
-  //   result.push(tens[fiveDigit]);
-  //   var fourDigit = Math.floor((numberEntered % 10000)/1000);
-  //   var remainder = numberEntered % 1000;
-  //   if (fourDigit !== 0) {
-  //     result.push(ordinals[fourDigit]);
-  //   }
-  //   result.push("thousand");
-  //   numberEntered = remainder;
-  // }
-  //
-  // if (numberEntered < 20000 && numberEntered > 999) {
-  //   var fourDigit = Math.floor(numberEntered / 1000);
-  //   var remainder = numberEntered % 1000;
-  //   if (fourDigit !== 0) {
-  //     result.push(ordinals[fourDigit]);
-  //   }
-  //   result.push("thousand");
-  //   numberEntered = remainder;
-  // }
-  //
-  // if (numberEntered < 1000 && numberEntered > 99) {
-  //   var threeDigit = Math.floor(numberEntered / 100);
-  //   result.push(ordinals[threeDigit])
-  //   var remainder = numberEntered % 100;
-  //   result.push("hundred");
-  //   numberEntered = remainder;
-  // }
-  //
-  // if (numberEntered < 100 && numberEntered > 19) {
-  //   var twoDigit = Math.floor(numberEntered / 10);
-  //   var remainder = numberEntered % 10;
-  //   result.push(tens[twoDigit]);
-  //   numberEntered = remainder;
-  // }
 
   if (numberEntered < 20 && numberEntered) {
     result.push(ordinals[numberEntered]);
@@ -71,13 +12,37 @@ var numbersInWords = function(numberEntered) {
     var numberSplit = numberEntered.toString().split("");
     result.unshift(ordinals[parseInt(numberSplit.pop())]);
     result.unshift(tens[(parseInt(numberSplit.pop()))]);
-      if (numberSplit !== 'undefined' && numberSplit.length > 0) {
-        result.unshift("hundred").unshift(ordinals[parseInt(numberSplit.pop())]);
-        var hundredChkr = parseInt(numberSplit.pop());
+      if (numberSplit.length > 0) {
+        result.unshift("hundred")
+        result.unshift(ordinals[parseInt(numberSplit.pop())]);
+
+        var hundredChkr = parseInt(result.pop());
           if (hundredChkr > 0 ) {
             result.unshift("hundred").unshift(ordinals[hundredChkr]);
+          } else {
+            result = result.slice(0, result.length - 1);
           }
       }
+      var biggieCounter = 0;
+      // do {
+      //   result.unshift(biggies[biggieCounter]);
+      //   threeAtATime = parseInt(numberSplit.pop(3).join());
+      //     if (threeAtATime > 0) {
+      //       result.unshift(biggies[biggieCounter]);
+      //     }
+      //     if (threeAtATime < 20) {
+      //       result.unshift(ordinals[threeAtATime]);
+      //     } else {
+      //       taatS = threeAtATime.toString().split("");
+      //       result.unshift(ordinals[parseInt(taatS.pop())]);
+      //       result.unshift(tens[parseInt(taatS.pop())]);
+      //         if (numberSplit !== 'undefined' && numberSplit.length > 0) {
+      //           result.unshift("hundred").unshift(ordinals[parseInt(taatS.pop())]);
+      //         }
+      //     }
+      //     biggieCounter = biggieCounter + 1;
+      // } while (numberSplit !== 'undefined' && numberSplit.length > 0)};
+
   }
 
   return result.join(" ");
